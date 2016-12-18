fdcd7cb1694c0e470083b8530abfb69b6b0230cb
Francesca Aguila <francescaaguila@gmail.com>
2016-12-18 09:00:00 +0800

    circle class R script

---
+++ circle_class.R
@@ -0,0 +1,19 @@
+#' An S3 class to represent a circle
+#' Circle
+#' @param radius, a number
+#' @return A circle object with a certain radius
+#' @export
+#' @examples
+#' circle(5)
+
+#' circle area() function
+#' 
+#' @return The area of a circle with radius \code{radius}
+#' @examples
+#' area()
+
+#' circle circumference() function
+#' 
+#' @return The circumference of a circle with radius \code{radius}
+#' @examples
+#' perimeter()
