40270c21c3dc10658d4a3983b810e8bd851d6cdd
David Laing <david.laing@alumni.ubc.ca>
2016-12-17 21:41:00 -0500

    implemented circle class

--- circle_class.R
+++ circle_class.R
@@ -5,15 +5,36 @@
 #' @export
 #' @examples
 #' circle(5)
+circle <- function(radius){
+        #set check
+        if (radius <= 0) stop('radius not in range')
+        if (is.na(radius)) stop('no input')
+        if (radius == Inf) stop('no infinite radii')
+        if (is.character(radius)) stop('no strings allowed')
+        if (is.list(radius)) stop('no lists allowed')
+        if (length(radius) != 1) stop('only one number please')
+        value <- list(radius=radius)
+        
+        #set class
+        attr(value, "class") <- "circle"
+        value
+}
 
 #' circle area() function
 #' 
 #' @return The area of a circle with radius \code{radius}
 #' @examples
 #' area()
+area.circle <- function(obj){
+        print(pi*obj$radius**2)
+}
+
 
 #' circle circumference() function
 #' 
 #' @return The circumference of a circle with radius \code{radius}
 #' @examples
-#' perimeter()
+#' circumference()
+circumference.circle <- function(obj){
+        print(2*pi*obj$radius)
+}
